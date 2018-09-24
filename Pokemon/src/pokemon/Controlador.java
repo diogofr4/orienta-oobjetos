1139b252c70a65dc25677c975d0bed1f02e3c128
ICE <ice@lab06m38.nrc.ice.ufjf.br>
2018-09-24 20:49:46 -0300

    adicionado os imports

--- Pokemon/src/pokemon/Controlador.java
+++ Pokemon/src/pokemon/Controlador.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pokemons;
-import pokemon.*;
+package pokemon;
 
 /**
  *
  * @author ice
  */
-public class Gastly {
+public class Controlador {
     
 }
